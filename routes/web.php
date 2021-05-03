1d4d4cf7f094491c4bfae26badb5f13d650249fd
fitriMD <mutiara.devi027@gmail.com>
2021-05-04 01:41:38 +0700

    Membuat Fitur Export PDF

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,8 @@ Route::resource('mahasiswas', MahasiswaController::class);
 Route::get('mahasiswa/nilai/{mahasiswa}', [MahasiswaController::class, 'nilai'])->name('mahasiswas.showNilai');
 Route::resource('articles', ArticleController::class);
 
+Route::get('/article/cetak_pdf', [ArticleController::class, 'cetak_pdf']);
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
